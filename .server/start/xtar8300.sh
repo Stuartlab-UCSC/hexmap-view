6292444752188b9fdb7fc57439554f1b1c56f0cf
swat <swat@darla.local>
2016-04-25 09:47:10 -0700

    cleaned up start scripts, fixed new user email

--- .server/start/xtar8300.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update this port with the tar-ball
-
-PORT=8300
-DIR=/cluster/home/swat/hexmap
-echo 'cleaning directory'
-rm -rf $DIR/$PORT/*
-echo 'untarring'
-tar -C $DIR/$PORT -x -f $DIR/t.tar
