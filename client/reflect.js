69e030e4a206f7ab09e68d645ae14e475a0f404a
swat <swat@darla.local>
2016-08-26 16:16:43 -0700

    added data type to the reflection dialog

--- client/reflect.js
+++ client/reflect.js
@@ -12,11 +12,11 @@ var app = app || {};
         $button,
         $dialog,
         mapId,
-        mapIdList,
         toMapId,
         toMapIds,
+        dataType,
+        dataTypes,
         selectionList,
-        toMapIdList,
         lastUser,
         subscribedToMaps = false,
         selectionSelected = ''; // option selected from the selection list
@@ -25,33 +25,48 @@ var app = app || {};
 
         // Show the contents of the dialog, once per trigger button click
  
-        //make Json for showing the possible maps to send reflection to
-        var data = [];
+        // Create the target map selector
+ 
+        // Put the data in the format the selector wants
+        var mapIdData = [];
         toMapIds.forEach(function(mapId){
-            data.push({id: mapId, text: mapId});
+            mapIdData.push({id: mapId, text: mapId});
         });
 
         var $mapAnchor = $('#reflectDialog .mapIdAnchor');
-        mapIdList = createOurSelect2($mapAnchor, {data: data}, 6);
+        createOurSelect2($mapAnchor, {data: mapIdData}, toMapId);
  
-        // TODO this should be set to the last selected toMapId
-        toMapId = toMapIds[0]
-        $mapAnchor.select2("val", toMapId);
- 
-        console.log('toMapId', toMapId);
-
         $mapAnchor.show();
 
-        // The selection selector.
+        // Define the event handler for selecting in the list
+        $mapAnchor.on('change', function (ev) {
+            toMapId = ev.target.value;
+            console.log('toMapId', toMapId);
+        });
+
+        // Create the layer name selector.
         selectionList = createLayerNameList($('#reflectDialog .layerNameListAnchor'),
                                     $('#reflectDialog .selectionListLabel'),
                                     selectionSelected);
         selectionList.enable(true, {binary: true});
 
-        // Define the event handler for the selecting in the list
-        $mapAnchor.on('change', function (ev) {
-            toMapId = ev.target.value;
+        // Create the data type selector
+ 
+        // Put the data in the format the selector wants
+        var dataTypeData = [];
+        dataTypes.forEach(function(type){
+            dataTypeData.push({id: type, text: type});
+        });
 
+        var $dataTypeAnchor = $('#reflectDialog .dataTypeAnchor');
+        createOurSelect2($dataTypeAnchor, {data: dataTypeData}, dataType);
+ 
+        $dataTypeAnchor.show();
+
+        // Define the event handler for selecting in the list
+        $dataTypeAnchor.on('change', function (ev) {
+            dataType = ev.target.value;
+            console.log('data type selected:', dataType);
         });
     }
 
@@ -105,7 +120,7 @@ var app = app || {};
  
     function hide() {
  
-        // TODO: do we need to free map select vars as well?
+        // Free some things, then hide the dialog
         selectionSelected = selectionList.selected;
         selectionList.destroy();
         selectionList = undefined;
@@ -127,6 +142,10 @@ var app = app || {};
         }
  
         if (addressEntry && toMapIds && toMapIds.length > 0) {
+ 
+            // We have map IDs, so initialize the selected target map
+            // and enable the trigger to open the reflection dialog
+            toMapId = toMapIds[0]
             $button.removeClass('disabled');
         } else {
             $button.addClass('disabled');
@@ -165,11 +184,17 @@ var app = app || {};
                 Meteor.subscribe('reflectionToMapIds',ctx.project, getToMapIds);
                 subscribedToMaps = true;
             }
+ 
+            // Duncan, I'll let you fill this in with a subscription or whatever.
+            // For now I'll just return a couple for testing. And you should
+            // initialize dataType something like I did for toMapId
+            dataTypes = ['mRNA', 'CNV'];
+            dataType = dataTypes[0];
 
             //keep track of windows open
             Meteor.subscribe('OpenedWindow',user.username,ctx.project);
-            // TODO: Create a listener to know when to save state ?
 
+            // Just before the page is closed, update the DB
             window.onbeforeunload = function() {
                 Meteor.subscribe('ClosedWindow',user.username,mapId);
             };
