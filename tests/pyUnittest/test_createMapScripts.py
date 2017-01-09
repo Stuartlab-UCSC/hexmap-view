6269197fb9d7301b9cf56cba54f52889732bb698
mrJeppard <duncmc831@gmail.com>
2017-01-08 18:55:12 -0800

    --spearman metric to compute_sparse more robust input to compute_sparse information about input to log in compute_sparse unit tests for compute_sparse full genomic matrix option available from layout.py

--- tests/pyUnittest/test_createMapScripts.py
+++ tests/pyUnittest/test_createMapScripts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 
-# This tests javascript, using python's easier calls to shell commands
+# This tests python, using python's easier calls to shell commands
 # from here than from mocha
 
 import sys, os, glob, filecmp, subprocess, json, tempfile, pprint, shutil
@@ -14,24 +14,84 @@ rootDir = getRootDir()
 testDir = rootDir + 'tests/pyUnittest/'
 inDir = testDir + 'createMapIn/'   # The input data
 outDir = testDir + 'createMapOut/' # The actual output data
+scriptDir = rootDir + 'www/server'
 
-#scriptDir = rootDir + 'www/server'
-#scriptDir = testDir + 'yulia'
-scriptDir = testDir + 'duncan'
-
-PYTHONPATH = rootDir + 'www/server'
-os.environ["PYTHONPATH"] = PYTHONPATH
+#http://stackoverflow.com/questions/3108285/in-python-script-how-do-i-set-pythonpath
+#point the python path to the script directory
+sys.path.append(scriptDir)
 
 import compute_sparse_matrix
-import compute_sparse_matrix_yulia
 
 class TestCreateMap(unittest.TestCase):
 
-    def test_similaritySparse6(s):
-    
-        # Test for sparse matrix with 6 top neighbors and zero num_jobs
+    def test_similarityTop20Spear(s):
+
+        # Test for sparse matrix with 20 top neighbors and spearman correlation
         baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
-        outName = baseName + '_top6.tab'
+        outName = baseName + '.top20.spearman.tab'
+        expDir = testDir + 'computeSimExp/' # The expected output data
+        opts = [
+            '--in_file', inDir + baseName + '.tab',
+            '--top', '20',
+            '--metric', 'spearman',
+            '--output_type', 'sparse',
+            '--out_file', outDir + outName,
+            '--log', outDir + 'log1',
+            '--num_jobs', '2'
+        ]
+
+        util.removeOldOutFiles(outDir)
+        rc = compute_sparse_matrix.main(opts)
+        s.assertTrue(rc == 0)
+        util.compareActualVsExpectedFile(s, outName, outDir, expDir)
+
+    def test_similarityTop20Pcorr(s):
+
+        # Test for sparse matrix with 20 top neighbors and pearson correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.top20.correlation.tab'
+        expDir = testDir + 'computeSimExp/' # The expected output data
+        opts = [
+            '--in_file', inDir + baseName + '.tab',
+            '--top', '20',
+            '--metric', 'correlation',
+            '--output_type', 'sparse',
+            '--out_file', outDir + outName,
+            '--log', outDir + 'log2',
+            '--num_jobs', '2'
+        ]
+
+        util.removeOldOutFiles(outDir)
+        rc = compute_sparse_matrix.main(opts)
+        s.assertTrue(rc == 0)
+        util.compareActualVsExpectedFile(s, outName, outDir, expDir)
+
+    def test_similarityTop6Spear(s):
+    
+        # Test for sparse matrix with 6 top neighbors and spearman correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.top6.spearman.tab'
+        expDir = testDir + 'computeSimExp/' # The expected output data
+        opts = [
+            '--in_file', inDir + baseName + '.tab',
+            '--top', '6',
+            '--metric', 'spearman',
+            '--output_type', 'sparse',
+            '--out_file', outDir + outName,
+            '--log', outDir + 'log3',
+            '--num_jobs', '2'
+        ]
+
+        util.removeOldOutFiles(outDir)
+        rc = compute_sparse_matrix.main(opts)
+        s.assertTrue(rc == 0)
+        util.compareActualVsExpectedFile(s, outName, outDir, expDir)
+
+    def test_similarityTop6Pcorr(s):
+
+        # Test for sparse matrix with 6 top neighbors and pearson correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.top6.correlation.tab'
         expDir = testDir + 'computeSimExp/' # The expected output data
         opts = [
             '--in_file', inDir + baseName + '.tab',
@@ -39,53 +99,96 @@ class TestCreateMap(unittest.TestCase):
             '--metric', 'correlation',
             '--output_type', 'sparse',
             '--out_file', outDir + outName,
-            '--log', outDir + 'log',
-            '--num_jobs', '0'
+            '--log', outDir + 'log4',
+            '--num_jobs', '2'
         ]
+
         util.removeOldOutFiles(outDir)
         rc = compute_sparse_matrix.main(opts)
         s.assertTrue(rc == 0)
         util.compareActualVsExpectedFile(s, outName, outDir, expDir)
 
-    def test_similarityFull3(s):
-    
-        # Test for full matrix with 3 top neighbors and 3 num_jobs
+    def test_similarityTop3Spear(s):
+
+        # Test for sparse matrix with 3 top neighbors and spearman correlation
         baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
-        outName = baseName + '_top3.tab'
+        outName = baseName + '.top3.spearman.tab'
+        expDir = testDir + 'computeSimExp/' # The expected output data
+        opts = [
+            '--in_file', inDir + baseName + '.tab',
+            '--top', '3',
+            '--metric', 'spearman',
+            '--output_type', 'sparse',
+            '--out_file', outDir + outName,
+            '--log', outDir + 'log5',
+            '--num_jobs', '2'
+        ]
+
+        util.removeOldOutFiles(outDir)
+        rc = compute_sparse_matrix.main(opts)
+        s.assertTrue(rc == 0)
+        util.compareActualVsExpectedFile(s, outName, outDir, expDir)
+
+    def test_similarityTop3Pcorr(s):
+
+        # Test for sparse matrix with 3 top neighbors and pearson correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.top3.correlation.tab'
         expDir = testDir + 'computeSimExp/' # The expected output data
         opts = [
             '--in_file', inDir + baseName + '.tab',
             '--top', '3',
             '--metric', 'correlation',
-            '--output_type', 'full',
+            '--output_type', 'sparse',
             '--out_file', outDir + outName,
-            '--log', outDir + 'log',
+            '--log', outDir + 'log6',
             '--num_jobs', '2'
         ]
+
         util.removeOldOutFiles(outDir)
         rc = compute_sparse_matrix.main(opts)
         s.assertTrue(rc == 0)
         util.compareActualVsExpectedFile(s, outName, outDir, expDir)
-    
-    def test_similarityAllSameValues(s):
-    
-        # Test for when all of the input values are the same
-        baseName = 'similarityAllSameValues'
-        outName = baseName + '_top6.tab'
+
+    def test_similarityFullPcorr(s):
+
+        # Test for full matrix with pearson correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.full.correlation.tab'
         expDir = testDir + 'computeSimExp/' # The expected output data
         opts = [
-            '--in_file', inDir + baseName,
-            '--top', '3',
+            '--in_file', inDir + baseName + '.tab',
             '--metric', 'correlation',
             '--output_type', 'full',
             '--out_file', outDir + outName,
-            '--log', outDir + 'log',
+            '--log', outDir + 'log7',
             '--num_jobs', '2'
         ]
+
         util.removeOldOutFiles(outDir)
         rc = compute_sparse_matrix.main(opts)
         s.assertTrue(rc == 0)
         util.compareActualVsExpectedFile(s, outName, outDir, expDir)
-    
+
+    def test_similarityFullSpear(s):
+
+        # Test for sparse matrix with 3 top neighbors and spearman correlation
+        baseName = 'TGCT_IlluminaHiSeq_RNASeqV2'
+        outName = baseName + '.full.spearman.tab'
+        expDir = testDir + 'computeSimExp/' # The expected output data
+        opts = [
+            '--in_file', inDir + baseName + '.tab',
+            '--metric', 'spearman',
+            '--output_type', 'full',
+            '--out_file', outDir + outName,
+            '--log', outDir + 'log8',
+            '--num_jobs', '2'
+        ]
+
+        util.removeOldOutFiles(outDir)
+        rc = compute_sparse_matrix.main(opts)
+        s.assertTrue(rc == 0)
+        util.compareActualVsExpectedFile(s, outName, outDir, expDir)
+
 if __name__ == '__main__':
     unittest.main()
