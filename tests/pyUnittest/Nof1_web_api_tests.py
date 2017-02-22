f209ebdc2852623ae9b70eb2b93741f1db8bddb1
mrJeppard <duncmc831@gmail.com>
2017-02-22 00:14:16 -0800

    added tests for function calls of Nof1 API

--- tests/pyUnittest/Nof1_web_api_tests.py
+++ tests/pyUnittest/Nof1_web_api_tests.py
@@ -3,6 +3,14 @@ import hub
 import unittest
 import tempfile
 import json
+from rootDir import getRootDir
+from Nof1_hub import *
+import newplacement
+
+rootDir = getRootDir()
+testDir = rootDir + 'tests/pyUnittest/'
+inDir = testDir + 'in/layout/'
+expDir = testDir + 'exp/layoutBasic/'
 
 class Nof1TestCase(unittest.TestCase):
 
@@ -38,6 +46,64 @@ class Nof1TestCase(unittest.TestCase):
             s.assertTrue('', 'no json data in response')
         s.assertTrue(rv.status_code == 400)
         s.assertTrue(data['error'] == 'Content-Type must be application/json')
+
+    def test_data_reading(s):
+        '''
+        test for Nof1_hubs data input into python structures
+        @return:
+        '''
+
+        #make expected input from json:
+        featMat = inDir+ 'mcrchopra.data.tab'
+        preSquig = expDir + '/xyPreSquiggle_0.tab'
+        fin = open(featMat,'r')
+        tabArr = []
+        for line in fin:
+            #put the line in the array minus the new line
+            tabArr.append(line[:-1])
+
+        refDF,xyDF,newNodesDF = putDataIntoPythonStructs(featMat,preSquig,tabArr)
+        s.assertTrue(refDF.shape == newNodesDF.shape,'reading of tab array incorrect')
+
+    def test_data_reading2(s):
+        '''
+        test for Nof1_hubs data input into python structures
+        @return:
+        '''
+
+        #make expected input from json:
+        featMat = inDir+ 'mcrchopra.data.tab'
+        preSquig = expDir + '/xyPreSquiggle_0.tab'
+        fin = open(featMat,'r')
+        tabArr = []
+        for line in fin:
+            #put the line in the array minus the new line
+            tabArr.append(line[:-1])
+
+        refDF,xyDF,newNodesDF = putDataIntoPythonStructs(featMat,preSquig,tabArr)
+        s.assertTrue(xyDF.shape == (60,2),'reading of xy positions incorrect')
+
+    def test_json_out(s):
+        '''
+        test for Nof1_hubs data input into python structures
+        @return:
+        '''
+
+        #make expected input from json:
+        featMat = inDir+ 'mcrchopra.data.tab'
+        preSquig = expDir + '/xyPreSquiggle_0.tab'
+        fin = open(featMat,'r')
+        tabArr = []
+        for line in fin:
+            #put the line in the array minus the new line
+            tabArr.append(line[:-1])
+
+        refDF,xyDF,newNodesDF = putDataIntoPythonStructs(featMat,preSquig,tabArr)
+
+        retDict = outputToJson(*newplacement.placeNew(newNodesDF,refDF,xyDF,6))
+        s.assertTrue(len(retDict['nodes'].keys()) == 60,
+                     'json output has wrong number of nodes:')
+
 
     def test_invalid_json(s):
         rv = s.app.post('/query/overlayNodes',
