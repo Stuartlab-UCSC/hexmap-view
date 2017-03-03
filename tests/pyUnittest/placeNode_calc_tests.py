05ee13eb4f3c00bd062d84184fbaa3bed904ffcf
mrJeppard <duncmc831@gmail.com>
2017-03-03 12:20:33 -0800

    test for same results for placeing 1 node or multiple nodes with same values

--- tests/pyUnittest/placeNode_calc_tests.py
+++ tests/pyUnittest/placeNode_calc_tests.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import tempfile
 import json
-
+import pandas as pd
 from rootDir import getRootDir
 import placeNode
 from placeNode_calc import *
@@ -10,6 +10,7 @@ from placeNode_calc import *
 rootDir = getRootDir()
 testDir = rootDir + 'tests/pyUnittest/'
 inDir = testDir + 'in/layout/'
+inDirCalc = testDir + 'in/placeNode/'
 expDir = testDir + 'exp/layoutBasic/'
 
 class Nof1CalcTestCase(unittest.TestCase):
@@ -74,6 +75,82 @@ class Nof1CalcTestCase(unittest.TestCase):
 
         s.assertTrue(len(retDict['nodes'].keys()) == 60,
                      'json output has wrong number of nodes:')
+    def test_calc_independence(s):
+        '''
+        Tests that doing 1 calc produces the same results as doing multiple
+         calculations
+        @return:
+        '''
+
+        #make expected input from json:
+        featMat = inDirCalc+ 'trimmedRPPAdata.tab'
+        nodesFile = inDirCalc + 'newNodesRPPA.tab'
+        nodeFile1 = inDirCalc + 'newNode1RPPA.tab'
+        nodeFile2 = inDirCalc + 'newNode2RPPA.tab'
+        preSquig = inDirCalc + 'xyPreSquiggle_2.tab'
+
+        nodesDictBoth = pd.read_csv(nodesFile,index_col=0,sep='\t').to_dict()
+        nodesDict1 = pd.read_csv(nodeFile1,index_col=0,sep='\t').to_dict()
+        nodesDict2 = pd.read_csv(nodeFile2,index_col=0,sep='\t').to_dict()
+
+        ##
+        #uses the same input function as the calc routine
+        # to get data into pandas structs
+        refDF,xyDF,newNodesDF = putDataIntoPythonStructs(featMat,
+                                                         preSquig,
+                                                         nodesDictBoth)
+
+        refDF,xyDF,newNode1 = putDataIntoPythonStructs(featMat,
+                                                         preSquig,
+                                                         nodesDict1)
+
+        refDF,xyDF,newNode2 = putDataIntoPythonStructs(featMat,
+                                                         preSquig,
+                                                         nodesDict2)
+        ##
+        #uses the same output function as the calc routine
+        retDictBoth = outputToDict(*placeNode.placeNew(newNodesDF,
+                                                   refDF,
+                                                   xyDF,
+                                                   6,
+                                                   'mapId'))
+
+        retDict1 = outputToDict(*placeNode.placeNew(newNode1,
+                                                   refDF,
+                                                   xyDF,
+                                                   6,
+                                                   'mapId'))
+
+        retDict2 = outputToDict(*placeNode.placeNew(newNode2,
+                                                   refDF,
+                                                   xyDF,
+                                                   6,
+                                                   'mapId'))
+        #
+
+        #make sure that the x and y coord  are the same
+        # whether doing the test individually or 2 nodes at the same time
+        s.assertTrue(
+            retDictBoth['nodes']['test1']['x'] == \
+            retDict1['nodes']['test1']['x'],
+                     'doing one test not independent of both'
+        )
+        s.assertTrue(
+            retDictBoth['nodes']['test1']['y'] == \
+            retDict1['nodes']['test1']['y'],
+                     'doing one test not independent of both'
+        )
+
+        s.assertTrue(
+            retDictBoth['nodes']['test2']['y'] == \
+            retDict2['nodes']['test2']['y'],
+                     'doing one test not independent of both'
+        )
+        s.assertTrue(
+            retDictBoth['nodes']['test2']['x'] == \
+            retDict2['nodes']['test2']['x'],
+                     'doing one test not independent of both'
+        )
 
 if __name__ == '__main__':
     unittest.main()
