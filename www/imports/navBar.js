482c0a67e51f8ed10e961d8e04a0d3b85ad5b7c2
swat <swat@darla.local>
2017-09-13 19:24:16 -0700

    on navBar, don't import files until the menu option is selected

--- www/imports/navBar.js
+++ www/imports/navBar.js
@@ -2,17 +2,20 @@
 // navBar.js
 // Create the menu event handlers.
 
-import AttrAdd from './reactCandidates/attrAdd.js';
-function attrAdd () {
-    $('#navBar .attrAdd').on('click', AttrAdd.init);
-}
-
-import NodeIdSelect from './nodeIdSelect.js';
-function nodeIdSelect () {
-    $('#navBar .nodeIdSelect').on('click', NodeIdSelect.show);
-}
 
 exports.init = function () {
-    attrAdd();
-    nodeIdSelect();
+    $('#navBar .attrAdd').on('click', function () {
+        import AttrAdd from './reactCandidates/attrAdd.js';
+        AttrAdd.init();
+    });
+
+    $('#navBar .nodeIdSelect').on('click', function () {
+        import NodeIdSelect from './nodeIdSelect.js';
+        NodeIdSelect.show();
+    });
+
+}
+
+}
+
 }
