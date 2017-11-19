5e50a747642b488274a8bf7f9b6d1401365178c2
Duncan McColl <duncmc831@gmail.com>
2017-11-19 15:45:13 -0800

    look for default settings

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # $1: /path/to/the/config/file, can be ommited if there is a
 # config/setup.ch in the current directory, or if $HEXMAP is defined on your machine.
+CONFIG_FILE=$1
 
-# Source the configuration file for this machine.
-if [ -z $1 ]; then
-    source $1
-elif [ -f $(pwd)/confing/setup.cf ]; then
-    echo "using the default config file, $(pwd)/config/setup.cf" ];
-elif [ -z $HEXMAP ]; then
-    echo "Using preset \$HEXMAP var: $HEXMAP"
+# Attempt to use default configuration if the argument has not been supplied
+if [ -z "$CONFIG_FILE" ] && [ -f "$(pwd)/config/setup.cf" ]; then
+    CONFIG_FILE=$(pwd)/config/setup.cf
 else
-    echo "Path to configuration file as first arg or defined \$HEXMAP necessary."
-    exit
+    echo "Path to configuration file as first arg necessary."
+    exit 1
 fi
+
+echo "Using config file: $CONFIG_FILE"
+source $CONFIG_FILE
 
 # Might need to make settings.json with your node file here....
 # Build a www server with compression and other production enhancements.
