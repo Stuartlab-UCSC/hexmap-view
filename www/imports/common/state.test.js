aacc7e560b5679b231903c04f4275b05bd04b472
swat <swat@darla.local>
2018-02-08 16:36:32 -0800

    update compute server install

---
+++ www/imports/common/state.test.js
@@ -0,0 +1,16 @@
+
+//add = require('./add');
+import overlayNodes from '/imports/mapPage/calc/overlayNodes';
+import rx from '/imports/common/rx';
+import shortlist from '/imports/mapPage/shortlist/shortlist';
+import urlParms from '/imports/common/urlParms';
+import util from '/imports/common/util';
+import utils from '/imports/common/utils';
+
+import state from './state';
+
+describe('load_background', () => {
+  it('should load background state from store', () => {
+    expect(state.load({'background': 'white'})).toBe('white');
+  });
+});
