60afd1727af07a996074124d575c7789dca14465
swat <>
2018-07-07 14:18:18 -0700

    cell atlas home page templates

---
+++ www/imports/cell/appSrc/index.js
@@ -0,0 +1,12 @@
+
+// A stub so the cell page may be developed independent of the tumor map
+// application.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cell from './Cell.js'
+
+ReactDOM.render(
+  <Cell />,
+  document.getElementById('root')
+);
