92738d0bb308126b30f8ee1c3667a70c101b7e44
swat <>
2018-07-20 13:14:08 -0700

    remove need for http proxy

--- bin/js/https.js
+++ bin/js/https.js
@@ -5,7 +5,6 @@ const httpProxy = require('http-proxy'),
 
 // Gather environment variables.
 const PROXY_PORT = process.env.HTTPS_PORT,
-    TARGET_PORT = process.env.PORT,
     KEY = process.env.KEY,
     CERT = process.env.CERT,
     PATH_TO_CHAIN = process.env.CHAIN,
@@ -24,89 +23,4 @@ var options = {
     xfwd: true
 };
 
-function timestamp () {
-
-    // This returns a timestamp of the form: Jan 26 2017 11:20:48:295
-    var now = new Date();
-    return now.toString().slice(4, -15) + ':' + now.getMilliseconds()
-}
-
-try {
-    var proxy = httpProxy.createProxyServer(options).listen(PROXY_PORT);
-    
-} catch (error) {
-    console.log(timestamp(), 'https proxy could not start because view server is not running:', TARGET_PORT);
-    return;
-}
-console.log(timestamp(), 'https proxy starting on', PROXY_PORT, 'targetting', TARGET_PORT);
-
-
-// Listen for the `error` event on `proxy`. 
-try {
-    proxy.on('error', function (err, req, res) {
-        try {
-            console.log(timestamp(), 'error: https.js err', err);
-            res.writeHead(500, {
-                'Content-Type': 'text/plain'
-            });         
-            res.end('Something went wrong with httpS:', err);
-        } catch (error) {
-            console.log(timestamp(), 'error: on processing error event because view server is not running:', TARGET_PORT);
-        }
-    });
-
-} catch (error) {
-    console.log(timestamp(), 'https proxy failed on "error" event because view server is not running:', TARGET_PORT);
-}
-
-
-// Listen for the `proxyRes` event on `proxy`.
-try {
-    proxy.on('proxyRes', function (proxyRes, req, res) {
-        try {
-            //console.log(timestamp(), 'info: RAW Response from target: connection:', proxyRes.headers.connection);
-        } catch (error) {
-            console.log(timestamp(), 'error: on processing proxyRes event because view server is not running:', TARGET_PORT);
-        }
-    });
-
-} catch (error) {
-    console.log(timestamp(), 'error: on "proxyRes" event because view server is not running:', TARGET_PORT);
-}
-
-
-// Listen for the `open` event on `proxy`.
-try {
-    proxy.on('open', function (proxySocket) {
-        try {
-            // listen for messages coming FROM the target here
-            proxySocket.on('data', function (data) {
-                //console.log(timestamp(), 'info: proxy on open');
-            });
-            
-        } catch (error) {
-            console.log(timestamp(), 'error: on processing open event because view server is not running:', TARGET_PORT);
-        }
-    });
-    
-} catch (error) {
-    console.log(timestamp(), 'error: on "open" event because view server is not running:', TARGET_PORT);
-}
-
-
-// Listen for the `close` event on `proxy`.
-try {
-    proxy.on('close', function (res, socket, head) {
-        try {
-            // view disconnected websocket connections
-            //console.log(timestamp(), 'info: proxy close event (client disconnected)');
-            
-        } catch (error) {
-            console.log(timestamp(), 'error: on processing close event because view server is not running:', TARGET_PORT);
-        }
-        
-    });
-    
-} catch (error) {
-    console.log(timestamp(), 'error: on "close" event because view server is not running:', TARGET_PORT);
-}
+var proxy = httpProxy.createProxyServer(options).listen(PROXY_PORT);
